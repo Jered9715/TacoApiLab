aab593cea47987436c7a62bad375bb1255b974af
Jered O'Grady <jered9715@gmail.com>
2024-05-06 05:28:26 -0400

    Got DB,Nuget packages, and scaffolding completed. default connection moved from context to appsettings.json

---
+++ TacoFastFoodApi/TacoFastFoodApi/Models/FastFoodTacoDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TacoFastFoodApi.Models;
+
+public partial class FastFoodTacoDbContext : DbContext
+{
+    public FastFoodTacoDbContext()
+    {
+    }
+
+    public FastFoodTacoDbContext(DbContextOptions<FastFoodTacoDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Drink> Drinks { get; set; }
+
+    public virtual DbSet<Taco> Tacos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Drink>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Drink__3214EC27E6B2060E");
+
+            entity.ToTable("Drink");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Taco>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Taco__3214EC2792ED6BF6");
+
+            entity.ToTable("Taco");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
